10da4355bbb212df7dee983c552521bd7a65f1c7
Leonardo Fanck <leonardofanck@gmail.com>
2024-02-14 22:33:38 -0300

    Desfeita a alteração anterior no banco de dados. Adicionado consultas gerais na tela de produtos.

--- SistemaDoLeo/SistemaDoLeo/Paginas/Produtos.xaml.cs
+++ SistemaDoLeo/SistemaDoLeo/Paginas/Produtos.xaml.cs
@@ -1,7 +1,10 @@
-﻿using SistemaDoLeo.Modelos.Classes;
+﻿using Newtonsoft.Json;
+using SistemaDoLeo.DB;
+using SistemaDoLeo.Modelos.Classes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,17 +17,54 @@ namespace SistemaDoLeo.Paginas
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Produtos : TabbedPage
     {
+        private int Status;
+        private int Visualizar = 0;
+        private int Cadastro = 1;
+        private int Editar = 2;
+
+        private List<Categoria> listaCategorias = new List<Categoria>();
+        private List<Produto> listaProdutos = new List<Produto>();
+
+        private readonly HttpClient _client;
+        private string urlCategoria = $"{Links.ip}/Categoria";
+        private string url = $"{Links.ip}/Produto";
+
         public Produtos()
         {
             InitializeComponent();
+
+            BindingContext = this;
+
+            CurrentPage = Children[0];
+
+            HttpClientHandler httpClientHandler = PermissaoDeCertificado.GetInsecureHandler();
+            _client = new HttpClient(httpClientHandler);
         }
 
         protected override async void OnAppearing()
         {
             base.OnAppearing();
 
-            //await CarregaCategorias();
-            //await CarregaLista();
+            await CarregaListaProdutos();
+            await CarregaCategorias();
+        }
+
+        private async Task CarregaCategorias()
+        {
+            var json = await _client.GetStringAsync(urlCategoria);
+            listaCategorias = JsonConvert.DeserializeObject<List<Categoria>>(json);
+            PkrCategoria.ItemsSource = listaCategorias;
+        }
+
+        private async Task CarregaListaProdutos()
+        {
+            RefreshV.IsRefreshing = true;
+
+            var json = await _client.GetStringAsync(url);
+            listaProdutos = JsonConvert.DeserializeObject<List<Produto>>(json);
+            CvListagem.ItemsSource = listaProdutos;
+
+            RefreshV.IsRefreshing = false;
         }
 
         private void SrcBuscar_TextChanged(object sender, TextChangedEventArgs e)
@@ -57,9 +97,9 @@ namespace SistemaDoLeo.Paginas
 
         }
 
-        private void RefreshV_Refreshing(object sender, EventArgs e)
+        private async void RefreshV_Refreshing(object sender, EventArgs e)
         {
-
+            await CarregaListaProdutos();
         }
     }
 }

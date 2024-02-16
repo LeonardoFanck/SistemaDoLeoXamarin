6c7c360ee5dc2716bd5bb202bae95958fde8fac9
Leonardo Fanck <leonardofanck@gmail.com>
2024-02-16 11:43:21 -0300

    Realizado ajustes de maxLenght em todos os campos.

--- SistemaDoLeo/SistemaDoLeo/App.xaml.cs
+++ SistemaDoLeo/SistemaDoLeo/App.xaml.cs
@@ -10,7 +10,7 @@ namespace SistemaDoLeo
             InitializeComponent();
 
             //MainPage = new NavigationPage(new MainPage());
-            MainPage = new NavigationPage(new Produtos());
+            MainPage = new NavigationPage(new Clientes());
         }
 
         protected override void OnStart()
